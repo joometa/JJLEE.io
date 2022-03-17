4938ea10bf0367b64f0d3f1c46a4d57590205b72
joometa <jjlee1623@gmail.com>
2022-03-17 23:23:30 +0900

    Feat : Add CategoryList

--- src/components/Main/index.ts
+++ src/components/Main/index.ts
@@ -1,2 +1,3 @@
 export { default as ProfileImage } from './ProfileImage'
 export { default as Introduction } from './Introduction'
+export { default as CategoryList } from './CategoryList'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import { GlobalStyle, Footer } from '@components/Common'
-import { Introduction } from '@components/Main'
+import { Introduction, CategoryList } from '@components/Main'
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
 `
 
 const IndexPage: FunctionComponent = function () {
@@ -14,6 +20,7 @@ const IndexPage: FunctionComponent = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
       <Footer />
     </Container>
   )
